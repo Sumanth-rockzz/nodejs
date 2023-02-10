31c985652e6c9bcb21fc8c1c2167ba4129eae6b0
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-02-10 13:44:01 +0530

    roters and filters

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express=require('express')
 const app=express();
+
+const adminroutes=require('./routes/admin')
+const shoproutes=require('./routes/shop')
 const bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
-app.use('/add-product',(req,res,next)=>{
-    res.send(`<form action="/product" method="POST"><input type="text" name="title">
-            <input type="text" name="size">
-            <button type="submit">Add product</button></form>`);
-});
-app.post('/product',(req,res,next)=>{
-     console.log(req.body);
-     res.redirect('/');
-});
-app.use('/',(req,res,next)=>{
-    res.send(`<h1>This is node js Project</h1>`);
-});
+
+
+
+app.use('/admin',adminroutes);
+app.use('/shop',shoproutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
+})
+
+
 app.listen(3000);
