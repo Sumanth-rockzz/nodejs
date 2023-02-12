3e25e286d29be18e969ec8a391ec459735db3523
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-02-12 21:33:24 +0530

    dynamtic routing part 1

--- models/product.js
+++ models/product.js
@@ -1,44 +1,48 @@
-const fs=require('fs');
-const path=require('path');
+const fs = require('fs');
+const path = require('path');
 
-
-const p=path.join(path.dirname(require.main.filename),
-'data',
-'products.json'
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
 );
-const getproductsfromfile=(cb)=>{
-   
-     fs.readFile(p,(err,data)=>{
-        if(err)
-        {
-            cb([]);
-        }
-        else{
-        cb(JSON.parse(data));
-        }
-    })
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    this.id=Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id,cb){
+    getProductsFromFile(products=>{
+      const product = products.find(p=>p.id===id);
+      cb(product);
+  })
 }
-
-
-module.exports=class Product{
- 
-    constructor(t){
-        this.title=t;
-    }
-    save(){
-       
-       getproductsfromfile(products=>{
-        products.push(this);
-        fs.writeFile(p,JSON.stringify(products),(err)=>{
-            if(err)
-
-          console.log(err);
-        })
-       });
-          
-    }
-    
-    static fetchAll(cb){
-       getproductsfromfile(cb);
-    }
-}
+};
