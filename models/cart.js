7e36e1e10d9fa875c8b856fba36149dc463244fa
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-02-13 13:21:59 +0530

    some features added for below commited changes

--- models/cart.js
+++ models/cart.js
@@ -15,22 +15,22 @@ module.exports=class Cart{
         cart=JSON.parse(fileContent);
         }
         //analyze the cart ==> find existing product
-        const existingProductIndex=cart.products.findIndex(prod=>prod.id===id);
+        const existingProductIndex=cart.products.findIndex(
+            prod=>prod.id===id
+            );
         const existingProduct=cart.products[existingProductIndex];
         let updatedProduct;
         //add new product/increase quantity
-        if(existingProductIndex){
+        if(existingProduct){
             updatedProduct={...existingProduct};
-            updatedProduct.qty=updatedProduct.qty+1;
+            updatedProduct.qty= updatedProduct.qty+1;
             cart.products=[...cart.products];
-          
-            
+            cart.products[existingProductIndex]=updatedProduct;
         }
         else{
-            updatedProduct={id:id,qty:1};
+            updatedProduct={id : id, qty:1};
             cart.products=[...cart.products,updatedProduct];
         }
-       
         cart.totalPrice= cart.totalPrice+ +productPrice;
        
         fs.writeFile(p,JSON.stringify(cart),err=>{
