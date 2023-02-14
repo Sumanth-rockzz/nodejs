a60b2dd902aebe3232a0434e92cf07c7f598e61e
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-02-14 16:14:43 +0530

    added add delete fetch functionality using query

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const mysql=require('mysql2');
 
 const pool=mysql.createPool({
     host:'localhost',
-    user:'name',
+    user:'root',
     database:'node-complete',
     password:'Sumanth@123'
 });
